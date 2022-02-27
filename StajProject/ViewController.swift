c20109a13935d55fc120b6376d1e69f0eacf7fec
aalroas <mr.a.alroas@gmail.com>
2022-02-27 23:42:15 +0300

    Initial Commit

---
+++ StajProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StajProject
+//
+//  Created by Burj Almaha on 27.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
