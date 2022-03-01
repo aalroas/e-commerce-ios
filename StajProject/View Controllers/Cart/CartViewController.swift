ec83021d255554c16b4c54f3d304892933e7cf76
aalroas <mr.a.alroas@gmail.com>
2022-03-01 16:34:53 +0300

    all project

---
+++ StajProject/View Controllers/Cart/CartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CartViewController.swift
+//  StajProject
+//
+//  Created by Burj Almaha on 28.02.2022.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
